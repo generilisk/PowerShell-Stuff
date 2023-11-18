b8c23891600324777098bc71b224f2615dc5e5b5
Will Hughes <generilisk@gmail.com>
2023-11-17 17:03:29 -0800

    Fixed more template folder stuff

--- Sample information and syntax.ps1
+++
@@ -1,4 +0,0 @@
-﻿$Words = "Collections of Letters"
-Clear-Host
-Write-Host "$Words are neat" #Double quotes expand variables into their stored values
-Write-Host '$Words are neat' #Single quotes are literal
---
+++ Templates/StringSyntax.ps1
@@ -0,0 +1,6 @@
+﻿$Words = "Collections of Letters"
+Clear-Host
+Write-Host "$Words are neat" #Double quotes expand variables into their stored values
+Write-Host '$Words are neat' #Single quotes are literal
+#"Collections of Letters are neat"
+#'$Words are neat'
