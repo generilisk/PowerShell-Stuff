9796d4e9cba6da53eb14f7174b093f722fb45f70
Will Hughes <generilisk@gmail.com>
2024-01-29 11:51:20 -0800

    Added ListGroupUsers

---
+++ ListGroupUsers.ps1
@@ -0,0 +1,15 @@
+# Prompt user for AD group name
+$groupName = Read-Host "Enter the Active Directory group name"
+
+# Get users in the specified group
+try {
+    $groupMembers = Get-ADGroupMember -Identity $groupName -Recursive | Select-Object Name | Sort-Object Name
+    if ($groupMembers.Count -eq 0) {
+        Write-Host "No users found in the specified group."
+    } else {
+        Write-Host "Users in the group '$groupName':"
+        $groupMembers | Format-Table -AutoSize
+    }
+} catch {
+    Write-Host "Error: $_"
+}
