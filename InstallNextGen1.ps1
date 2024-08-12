0b755bb3d8f10f0438a4a949c6dd31a6b2edb53e
Will Hughes <generilisk@gmail.com>
2024-08-12 10:24:22 -0700

    New attempt at Sqlnclix64.msi

--- InstallNextGen1.ps1
+++ InstallNextGen1.ps1
@@ -2,7 +2,7 @@
 Start-Process -FilePath "\\shastahealth.org\shared\ITS\Store\Software\ODBC\17\VC_redist.x64.exe" -ArgumentList "/q" -Verb RunAs -Wait
 
 # Step 2: Install Sqlnclix64.msi -> Redo this one
-Start-Process -FilePath "msiexec.exe" -ArgumentList "/i \\shastahealth.org\shared\ITS\Store\Software\ODBC\17\Sqlnclix64.msi /qn" -Wait
+Start-Process -FilePath "msiexec.exe" -ArgumentList "/i `"`\\shastahealth.org\shared\ITS\Store\Software\ODBC\17\Sqlnclix64.msi`"` /qn" -Wait
 
 # Step 3: Run Create_ODBC.exe from Prod folder
 Start-Process -FilePath "\\shastahealth.org\shared\ITS\Store\Software\ODBC\17\Prod\Create_ODBC.exe" -Wait
