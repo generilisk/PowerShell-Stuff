d38d6a080b8b6038118e697fb19cfaa818ec4beb
Will Hughes <generilisk@gmail.com>
2024-12-03 15:25:57 -0800

    See previous.

--- LobBee Setup 2.ps1
+++
@@ -1,5 +0,0 @@
-﻿reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System /v DisableLockWorkstation /d 0x1 /t REG_DWORD
-reg add 'HKCU\Control Panel\Desktop' /v ScreenSaverIsSecure /d 1 /t REG_SZ
-
-net localgroup "Administrators" "schc\LobbyUser" /delete
-shutdown -t 0 -r
