abdc213288a6f8469e169801e0055b75c14ac4a9
Will Hughes <generilisk@gmail.com>
2026-09-25 13:52:35 -0700

    Remove InstallNextGen1/2 and closeNextGen -- NextGen is going away

--- closeNextGen.ps1
+++
@@ -1,3 +0,0 @@
-﻿$User = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
-$NGParts = ("NGAppLauncher","NextGenICS","qsi_cps","NextGenEMR","NGFileMaint","NGLicenseManager","NextGenEPM","NextGenEPMRptSvr","RosettaHoldingTank","NGSetDB","NGSystemAdmin","TemplateEditor","TemplateImportExport")
-Get-Process -Name $NGParts -IncludeUserName | Where-Object {$User -eq $_.username} | Stop-Process
